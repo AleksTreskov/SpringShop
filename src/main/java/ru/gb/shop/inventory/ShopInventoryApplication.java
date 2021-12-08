88baebf22079c9d1a013f37f72828a74960cce12
Aw0ken0 <82842195+Aw0ken0@users.noreply.github.com>
2021-12-08 23:21:16 +0300

    Practice Lesson9
    Added DB connection and roles

---
+++ src/main/java/ru/gb/shop/inventory/ShopInventoryApplication.java
@@ -0,0 +1,12 @@
+package ru.gb.shop.inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopInventoryApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ShopInventoryApplication.class, args);
+
+    }
+}
---
+++ target/classes/roles.sql
@@ -0,0 +1,3 @@
+insert into ROLES values (1,'ADMIN');
+insert into ROLES values (2,'MODERATOR');
+insert into ROLES values (3,'USER');
